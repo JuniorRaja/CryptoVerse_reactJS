9503bfc61a5a99b786f423e02957e7b99a7e8c19
Prasanna Rajendran <imprasannarajendran@gmail.com>
2022-12-11 19:38:05 +0530

    Base Setup Fin

--- src/App.js
+++ src/App.js
@@ -1,9 +1,66 @@
 import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
+import {
+  Navbar,
+  Exchanges,
+  Homepage,
+  Cryptocurrencies,
+  CryptoDetails,
+  News,
+} from "./components";
+import "./App.css";
 
 const App = () => {
   return (
-    <div>
-      <h1>CryptoVerse</h1>
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+        <Layout>
+          <div className="route">
+            <Routes>
+              <Route path="/" element={<Homepage />} />
+              <Route path="/exchanges" element={<Exchanges />} />
+              <Route path="/cryptocurrencies" element={<Cryptocurrencies />} />
+              <Route path="/crypto/:coinId" element={<CryptoDetails />} />
+              <Route path="/news" element={<News />} />
+              {/* 
+              <Route path="/">
+                <Homepage />
+              </Route>
+              <Route path="/exchanges">
+                <Exchanges />
+              </Route>
+              <Route path="/cryptocurrencies">
+                <Cryptocurrencies />
+              </Route>
+              <Route path="/crypto/:coinId">
+                <CryptoDetails />
+              </Route>
+              <Route path="/news">
+                <News />
+              </Route> */}
+            </Routes>
+          </div>
+        </Layout>
+        <div className="footer">
+          <Typography.Title
+            level={5}
+            style={{ color: "white", textAlign: "center" }}
+          >
+            CryptoVerse - Crypto Tracker
+            <br />
+            All Rights Reserved.
+          </Typography.Title>
+          <Space>
+            <Link to="/">Home</Link>
+            <Link to="/exchanges">Exchanges</Link>
+            <Link to="/news">News</Link>
+          </Space>
+        </div>
+      </div>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 import App from "./App";
+// import "antd/dist/antd.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// ReactDOM.render(
+//   <Router>
+//     <App />
+//   </Router>,
+//   document.getElementById("root")
+// );
+
+import { createRoot } from "react-dom/client";
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(
+  <Router>
+    <App tab="home" />
+  </Router>
+);
